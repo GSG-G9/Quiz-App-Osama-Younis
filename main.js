59a76e6a5526cb799882bdcfebbaff9d216ecf65
Osama-you <osaam.w.younis@gmail.com>
2020-10-21 11:48:19 +0300

    startQuizTimer, getQuestionListbyNo, and part of creation the page  in createquiztionPage is working now

--- main.js
+++ main.js
@@ -5,11 +5,79 @@ let insrtPointTxt =
         - There are many variations of passages of Lorem Ipsu.
         - It look like readable English. Many desktop in publishin`
 
+let answersList ={
+        1: {
+                question: "1 What is the formula for finding the volume of a cube?",
+                answers: ["2x*x", "E^3", "Fish", "2*x=3x(3+4)"],
+                rightAnswerNo: 4
+        },
+        2: {
+                question: "2 What is the formula for finding the volume of a cube?",
+                answers: ["2x*x", "E^3", "Fish", "2*x=3x(3+4)"],
+                rightAnswerNo: 4
+        },
+        3: {
+                question: "3 What is the formula for finding the volume of a cube?",
+                answers: ["2x*x", "E^3", "Fish", "2*x=3x(3+4)"],
+                rightAnswerNo: 4
+        },
+        4: {
+                question: "4 What is the formula for finding the volume of a cube?",
+                answers: ["2x*x", "E^3", "Fish", "2*x=3x(3+4)"],
+                rightAnswerNo: 4
+        },
+        5: {
+                question: "5 What is the formula for finding the volume of a cube?",
+                answers: ["2x*x", "E^3", "Fish", "2*x=3x(3+4)"],
+                rightAnswerNo: 4
+        },
+        6: {
+                question: "6 What is the formula for finding the volume of a cube?",
+                answers: ["2x*x", "E^3", "Fish", "2*x=3x(3+4)"],
+                rightAnswerNo: 4
+        },
+        7: {
+                question: "7 What is the formula for finding the volume of a cube?",
+                answers: ["2x*x", "E^3", "Fish", "2*x=3x(3+4)"],
+                rightAnswerNo: 4
+        },
+        8: {
+                question: "8 What is the formula for finding the volume of a cube?",
+                answers: ["2x*x", "E^3", "Fish", "2*x=3x(3+4)"],
+                rightAnswerNo: 4
+        },
+        9: {
+                question: "9 What is the formula for finding the volume of a cube?",
+                answers: ["2x*x", "E^3", "Fish", "2*x=3x(3+4)"],
+                rightAnswerNo: 4
+        },
+        10: {
+                question: "10 What is the formula for finding the volume of a cube?",
+                answers: ["2x*x", "E^3", "Fish", "2*x=3x(3+4)"],
+                rightAnswerNo: 4
+        },
+        11: {
+                question: "11 What is the formula for finding the volume of a cube?",
+                answers: ["2x*x", "E^3", "Fish", "2*x=3x(3+4)"],
+                rightAnswerNo: 4
+        },
+        12: {
+                question: "12 What is the formula for finding the volume of a cube?",
+                answers: ["2x*x", "E^3", "Fish", "2*x=3x(3+4)"],
+                rightAnswerNo: 4
+        },
+
+}
+
+let quizSetting = {
+        questionTimeBySecond: 30,
+        questionNo: 10
+}
 
 // //Transfer from the home to Quiz Page
-homeLeft.addEventListener("click",() => startQuizPage(insrtTitTxt, insrtPointTxt))
-homeLeftImg.addEventListener("click",() =>  startQuizPage(insrtTitTxt, insrtPointTxt));
-homeLeftTxt.addEventListener("click",() =>  startQuizPage(insrtTitTxt, insrtPointTxt));
+homeLeft.addEventListener("click",() => startQuizPage(insrtTitTxt, insrtPointTxt, answersList, quizSetting))
+homeLeftImg.addEventListener("click",() =>  startQuizPage(insrtTitTxt, insrtPointTxt, answersList, quizSetting));
+homeLeftTxt.addEventListener("click",() =>  startQuizPage(insrtTitTxt, insrtPointTxt, answersList, quizSetting));
 
 //Transfer from the home to Leaderboard Page
 homeRight.addEventListener("click", startLeaderboardPage);
--- view/quiz.js
+++ view/quiz.js
@@ -1,20 +1,9 @@
 let main = document.querySelector("#main")
-
-// START the quiz page
-let startQuizPage = (insrtTitTxt, insrtPointTxt) => {
-    main.innerHTML=""
-
-    // create the main div call quiz
-    let quiz =document.createElement("div")
-    quiz.id="quiz"
-    main.appendChild(quiz);
-
-    // create Instructions page
-    createInsrtPage(quiz, insrtTitTxt, insrtPointTxt)
-}
+let questionNo = 1
+const questionListbyNo = {}
 
 // create Instructions page
-let createInsrtPage = (mainDiv, quizTitleTxt, insrtPointTxt) => {
+let createInsrtPage = (mainDiv, quizTitleTxt, insrtPointTxt, answersList, quizSetting) => {
     let maintxt =document.createElement("div")
     maintxt.classList.add("maintxt")
     mainDiv.appendChild(maintxt);
@@ -41,11 +30,118 @@ let createInsrtPage = (mainDiv, quizTitleTxt, insrtPointTxt) => {
     nextBtn.style.cursor = "pointer"
     quiz.appendChild(nextBtn);
 
-    nextBtn.addEventListener("click", createQuestionPage)
+    nextBtn.addEventListener("click", () => createquiztionPage (answersList, quizSetting, questionNo))
     
 }
 
-let createQuestionPage = () =>{
+// create Quiztion Page by No. of Quiztion
+let createquiztionPage = (answersList, quizSetting, questionNo) =>{
+    questionNo = questionNo
+    if(questionNo == 1){
+        getQuestionListbyNo(answersList, quizSetting.questionNo)
+        console.log(questionListbyNo);
+    }
+
+    // clear Instructions page
     quiz.innerHTML=""
-    console.log("createQuestionPage");
+
+    // setup the timer
+    let quizTimer =document.createElement("div")
+    quizTimer.id = "quizTimer"
+    quiz.appendChild(quizTimer);
+    let seconds = quizSetting.questionTimeBySecond
+    quizTimer.innerText = `00:${seconds}`;  
+    startquizTimer(quizTimer, seconds)
+
+    // setup question No. Text
+    let quizNo =document.createElement("h1")
+    quizNo.id = "quiz-No"
+    quiz.appendChild(quizNo);
+    let quizNoTxt=""
+    switch (questionNo) {
+        case 1:
+            quizNoTxt ="1'st"
+            break;
+        case 2:
+            quizNoTxt ="2'nd"
+            break;
+        case 3:
+            quizNoTxt ="3'rd"
+            break;
+        default:
+            quizNoTxt = questionNo + "'th"
+            break;
+    }
+    quizNoTxt+=" Question"
+    // display it  
+    quizNo.innerHTML=quizNoTxt
+
+    //setup the question
+    let quizTxt =document.createElement("h1")
+    quizTxt.id = "quizTxt"
+    quiz.appendChild(quizTxt);
+    quizTxt.innerText = questionListbyNo[questionNo].question
+
+    // create answer faild and fill it
+    let temp = []
+    for (let i = 0; i < 4; i++) {
+        temp.push(document.createElement("div"))
+        answer = temp[i]
+        answer.id = `answers${i+1}`
+        quiz.appendChild(answer);
+        answer.innerText = questionListbyNo[questionNo].answers[i]
+    }
+
+    console.log("createquiztionPage");
 } 
+
+// get list of question by the No. that needed
+let getQuestionListbyNo = (answersList, questionNo) =>{
+    
+    let answersListLength = Object.values(answersList).length
+    let arrayOfNoChosenQuestion = []
+
+    for (let i = 0; i < questionNo; i++) {
+        let x = Math.floor(Math.random() * answersListLength);
+        if(arrayOfNoChosenQuestion.indexOf(x) == -1){
+            arrayOfNoChosenQuestion.push(x)
+            questionListbyNo[i]=answersList[x]
+        }else{
+            i=i-1
+        }
+    }
+}
+
+let startquizTimer = (quizTimer ,questionTimeBySecond) => {
+
+    let seconds = questionTimeBySecond 
+
+    var x = setInterval(function() { 
+        seconds = seconds-1
+        quizTimer.innerText = (seconds < 10)? `00:0${seconds}` : `00:${seconds}`;  
+        if (seconds < 0) { 
+            clearInterval(x); 
+            quizTimer.innerText = '00:00'; 
+            doWhenQuestionDone()
+        }
+    }, 1000); 
+}
+
+
+doWhenQuestionDone = () => {
+
+}
+
+
+// START the quiz page
+let startQuizPage = (insrtTitTxt, insrtPointTxt, answersList, quizSetting) => {
+    main.innerHTML=""
+
+    // create the main div call quiz
+    let quiz =document.createElement("div")
+    quiz.id="quiz"
+    main.appendChild(quiz);
+
+    // create Instructions page
+    createInsrtPage(quiz, insrtTitTxt, insrtPointTxt, answersList, quizSetting)
+}
